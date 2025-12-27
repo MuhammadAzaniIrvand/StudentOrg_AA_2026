b50ad0bdc14a4ff4a1b1f8ef7523a40d2981cc62
MuhammadAzaniIrvand <muhammad.azaniirvand@gmail.com>
2025-12-27 21:47:38 +0700

    fix: routes psda

--- frontend/src/routes/PSDARoutes.tsx
+++ frontend/src/routes/PSDARoutes.tsx
@@ -11,17 +11,35 @@ const PSDARoutes: React.FC = () => {
   return (
     <PSDALayout>
       <Routes>
-        <Route path="/" element={<PSDADashboard />} />
-        <Route path="/events" element={<PSDAEvents />} />
-        <Route path="/events/new" element={<PSDAEventForm />} />
-        <Route path="/events/:id" element={<PSDAEventForm />} />
-        <Route path="/applications" element={<PSDAApplications />} />
-        <Route path="/applications/:id" element={<PSDAApplicationDetail />} />
+        {/* 
+          PERBAIKAN: 
+          Karena di App.tsx sudah ada "/psda/*", 
+          maka di sini tidak perlu tulis "/psda" lagi.
+        */}
+
+        {/* 1. Menangani saat user membuka "/psda" saja (tanpa slash apapun) */}
+        <Route index element={<PSDADashboard />} />
+
+        {/* 2. Menangani "/psda/dashboard". Cukup tulis "dashboard" */}
+        <Route path="dashboard" element={<PSDADashboard />} />
+
+        {/* 3. Menangani "/psda/events" */}
+        <Route path="events" element={<PSDAEvents />} />
+
+        {/* 4. Menangani "/psda/events/new" */}
+        <Route path="events/new" element={<PSDAEventForm />} />
+
+        {/* 5. Menangani "/psda/events/:id" */}
+        <Route path="events/:id" element={<PSDAEventForm />} />
+
+        {/* 6. Menangani "/psda/applications" */}
+        <Route path="applications" element={<PSDAApplications />} />
+
+        {/* 7. Menangani "/psda/applications/:id" */}
+        <Route path="applications/:id" element={<PSDAApplicationDetail />} />
       </Routes>
     </PSDALayout>
   );
 };
 
 export default PSDARoutes;
-
-
